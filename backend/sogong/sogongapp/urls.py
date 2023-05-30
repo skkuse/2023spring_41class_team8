5ba53bf6833dc39b1a4f893b4d01e6f9c8b73659
Nebula622 <minsukdamin@gmail.com>
2023-05-30 22:36:31 +0900

    urls.py 변경사항 커밋

--- backend/sogong/sogongapp/urls.py
+++ backend/sogong/sogongapp/urls.py
@@ -4,12 +4,13 @@ from . import views
 urlpatterns = [
     path("user", views.register_view, name="register_view"),
     path("user/login", views.register_view, name='login_view'),
-    path("user/login", views.userinfo_view, name='userinfo_view'),
-    path("user/login", views.ethics_view, name='ethics_view'),
-    path("user/login", views.ethics_submission, name='ethics_submission'),
-    path("user/login", views.codings_view, name='codings_view'),
-    path("user/login", views.coding_answer, name='coding_answer'),
-    path("user/login", views.useranswer_view, name='useranswer_view'),
+    path("user", views.userinfo_view, name='userinfo_view'),
+    path("user", views.user_newinfo, name='user_newinfo'),
+    path("ethics", views.ethics_view, name='ethics_view'),
+    path("ethics/submission", views.ethics_submission, name='ethics_submission'),
+    path("codings", views.codings_view, name='codings_view'),
+    path("codings/answer", views.coding_answer, name='coding_answer'),
+    path("codings/submission", views.useranswer_view, name='useranswer_view'),
     # 다른 URL 패턴들
     # ...
 ]
