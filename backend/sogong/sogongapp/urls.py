b3cb16d352c90942def3f04e9d07ff5307c1d9e5
ilwoong100 <alltun100@g.skku.edu>
2023-05-30 17:04:47 +0900

    prompt modified

--- backend/sogong/sogongapp/urls.py
+++ backend/sogong/sogongapp/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path("register/", views.register_view, name="register"),
+    path("user/login/", views.userinfo_view, name='userinfo_view')
     # 다른 URL 패턴들
     # ...
 ]
