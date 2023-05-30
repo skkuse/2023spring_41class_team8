47a2cc2b54e77bf460bc0757118830b963c6d5a4
JYS1998 <coogbgb@naver.com>
2023-05-30 17:51:49 +0900

    AI coding user solved update

--- fe/src/pages/codingProblem/codingProblem.jsx
+++ fe/src/pages/codingProblem/codingProblem.jsx
@@ -80,6 +80,11 @@ function CodingProblem({ getUserInfo, updateUserInfo }) {
   const handleSaveAnswer = () => {
     setIsSubmitted(true);
     updateUserAnswer(code);
+    let userInfo = getUserInfo();
+      updateUserInfo({
+        ...userInfo,
+        solvedCodingProblems: [...userInfo.solvedCodingProblems, data.pid],
+      });
   };
   // 코드 숨기기/보이기 기능을 처리하는 함수
   const handleBlur = () => {
