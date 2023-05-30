8aacf8104d34c0a2af9c28e392c31b21d3a89168
Nebula622 <minsukdamin@gmail.com>
2023-05-31 00:16:45 +0900

    urls.py commit

--- backend/sogong/sogongapp/urls.py
+++ backend/sogong/sogongapp/urls.py
@@ -2,8 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("register/", views.register_view, name="register"),
-    path("user/login/", views.userinfo_view, name='userinfo_view')
-    # 다른 URL 패턴들
-    # ...
+    path("register", views.register_view, name="register_view"),
+    path("user_idcheck", views.user_idcheck, name="user_idcheck"),
+    path("user", views.register_view, name='login_view'),
+    path("ethics", views.ethics_view, name='ethics_view'),
+    path("ethics/submission", views.ethics_submission, name='ethics_submission'),
+    path("codings", views.codings_view, name='codings_view'),
+    path("codings/answer", views.coding_answer, name='coding_answer'),
+    path("codings/submission", views.useranswer_view, name='useranswer_view'),
 ]
