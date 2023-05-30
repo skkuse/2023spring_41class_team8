1d191bedf4cdad82b5b10406a9b2546a943f4c66
Nebula622 <minsukdamin@gmail.com>
2023-05-30 22:25:56 +0900

    urls.py update

--- backend/sogong/sogongapp/urls.py
+++ backend/sogong/sogongapp/urls.py
@@ -3,8 +3,13 @@ from . import views
 
 urlpatterns = [
     path("user", views.register_view, name="register_view"),
-    path("user/login", views.register_view, name='login_view')
-    
+    path("user/login", views.register_view, name='login_view'),
+    path("user/login", views.userinfo_view, name='userinfo_view'),
+    path("user/login", views.ethics_view, name='ethics_view'),
+    path("user/login", views.ethics_submission, name='ethics_submission'),
+    path("user/login", views.codings_view, name='codings_view'),
+    path("user/login", views.coding_answer, name='coding_answer'),
+    path("user/login", views.useranswer_view, name='useranswer_view'),
     # 다른 URL 패턴들
     # ...
 ]
